5fbcdf90b789ddac3682bd6f719b8223578ba87c
Felix Lindner <felix@existenzquantor.info>
2020-03-08 13:43:37 +0100

    more efficient, coffeeRobot example

--- core/causality.pl
+++ core/causality.pl
@@ -40,63 +40,61 @@ max_holds_since(Program, Fact, MaxCur, Max) :-
 contrast_program1(A1 : A2, CP1 : CP2) :- 
     contrast_program1(A1, CP1), 
     contrast_program1(A2, CP2).
-contrast_program1(A, A) :- action(A).
 contrast_program1(A, empty) :- action(A).
+contrast_program1(A, A) :- action(A), !.
 
 /* Compute programs with action-specific substitution by empty actions */
 contrast_program1(A1 : A2, CP1 : CP2, Act) :- 
     action(Act),
-    contrast_program1(A1, CP1, Act), 
+    contrast_program1(A1, CP1, Act),
     contrast_program1(A2, CP2, Act).
-contrast_program1(A, A, _) :- action(A).
-contrast_program1(A, empty, Act) :- 
-    action(Act), 
-    A == Act.
+contrast_program1(Act, empty, Act) :- action(Act).
+contrast_program1(A, A, _) :- action(A), !.
 
 /* Compute programs with contrast actions */
 contrast_program2(A1 : A2, CP1 : CP2) :- 
     contrast_program2(A1, CP1), 
     contrast_program2(A2, CP2).
-contrast_program2(A, A) :- action(A).
 contrast_program2(A, C) :-
     action(A), 
     contrast(A, CL), 
     member(C, CL).
+contrast_program2(A, A) :- action(A), !.
 
 /* Compute programs with with action-specific substitution contrast actions */
 contrast_program2(A1 : A2, CP1 : CP2, Act) :-
     action(Act),
     contrast_program2(A1, CP1, Act), 
     contrast_program2(A2, CP2, Act).
-contrast_program2(A, A, _) :- action(A).
 contrast_program2(A, C, Act) :- 
     A == Act,
     contrast(A, CL), 
     member(C, CL).
+contrast_program2(A, A, _) :- action(A), !.
 
 /* Compute programs with empty or contrast actions */
 contrast_program3(A1 : A2, CP1 : CP2) :- 
     contrast_program3(A1, CP1), 
     contrast_program3(A2, CP2).
-contrast_program3(A, A) :- action(A).
 contrast_program3(A, empty) :- action(A).
 contrast_program3(A, C) :- 
     action(A), 
     contrast(A, CL), 
     member(C, CL).
+contrast_program3(A, A) :- action(A), !.
 
 /* Compute programs with action-specific substitution by empty or contrast actions */
 contrast_program3(A1 : A2, CP1 : CP2, Act) :- 
     action(Act),
     contrast_program3(A1, CP1, Act), 
     contrast_program3(A2, CP2, Act).
-contrast_program3(A, A, _) :- action(A).
 contrast_program3(A, empty, Act) :- 
     A == Act.
 contrast_program3(A, C, Act) :- 
     A == Act, 
     contrast(A, CL), 
     member(C, CL).
+contrast_program3(A, A, _) :- action(A), !.
 
 /* But-For Cause */
 but_for_cause(Program, Action, Fact, CP) :-
--- examples/coffeeRobot.pl
+++ examples/coffeeRobot.pl
@@ -18,8 +18,17 @@ goal([
 ]).
 
 effect(brew, [not(coff1)], [coff1]).
-effect(brew, [not(coff2)], [coff2]).
-effect(brew, [not(coff3)], [coff3]).
+effect(brew, [coff1, not(coff2)], [coff2]).
+effect(brew, [coff2, not(coff3)], [coff3]).
 effect(serve, [coff3], [not(coff3)]).
-effect(serve, [coff2], [not(coff2)]).
-effect(serve, [coff1], [not(coff1)]).
+effect(serve, [coff2, not(coff3)], [not(coff2)]).
+effect(serve, [coff1, not(coff2)], [not(coff1)]).
+effect(serve, [locDining, not(coffMrSmith), coffMrsSmith], [coffMrSmith]).
+effect(serve, [locDining, not(coffMrsSmith)], [coffMrsSmith]).
+effect(serve, [locBalcony, not(coffMrsBrown)], [coffMrsBrown]).
+effect(enterKitchen, [locHall], [not(locHall), locKitchen]).
+effect(leaveKitchen, [locKitchen], [not(locKitchen), locHall]).
+effect(enterDining, [locHall], [not(locHall), locDining]).
+effect(leaveDining, [locDining], [not(locDining), locHall]).
+effect(enterBalcony, [locHall], [not(locHall), locBalcony]).
+effect(leaveBalcony, [locBalcony], [not(locBalcony), locHall]).
